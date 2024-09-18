b2d82a69ee8c07af1ff9addf9e1f6c0cba7878c4
Shreyash Raj <shreyashr267@gmail.com>
2024-09-18 19:30:43 +0530

    boom boom ciao

---
+++ java/README.md
@@ -0,0 +1,24 @@
+# Java Learning Journey
+So to learn java I'm planning to solve 5 questions from each topic.
+
+By 5 questions I mean 5 complete question without any help from that topic.
+
+## Topics
+### Array
+### Hash Table
+### Two Pointers
+### Stack
+### Binary Search
+### Sliding Window
+### Linked List
+### Trees
+### Tries
+### Backtracking
+### Heap
+### Priority Queue
+### Graphs
+### Intervals
+### Greedy
+### Dynamic Programming
+### Bit Manipulation
+### Math
---
+++ python3/543.diameter-of-binary-tree.py
@@ -0,0 +1,29 @@
+#
+# @lc app=leetcode id=543 lang=python3
+#
+# [543] Diameter of Binary Tree
+#
+
+# @lc code=start
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
+        res = [0]
+
+        def dfs(root):
+            if not root:
+                return 0
+            
+            left = 1 + dfs(root.left)
+            right = 1 + dfs(root.right)
+
+            return max(left, right)
+        
+        return dfs(root)
+# @lc code=end
+
